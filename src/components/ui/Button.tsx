2fdd5f13aa1794780b2876565e76ea5da31586e4
hungerbk <wjantlr@gmail.com>
2026-05-11 21:15:21 +0900

    refactor: theme 변수명 미적용 부분 적용

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,15 +1,12 @@
 type Props = {
-  children: React.ReactNode
-  className?: string
-} & React.ButtonHTMLAttributes<HTMLButtonElement>
+  children: React.ReactNode;
+  className?: string;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function Button({ children, className, ...buttonProps }: Props) {
   return (
-    <button
-      className={`py-2 rounded-lg bg-(--color-primary) text-white font-semibold hover:opacity-90 active:opacity-80 transition-opacity ${className ?? ""}`}
-      {...buttonProps}
-    >
+    <button className={`py-2 rounded-lg bg-primary text-white font-semibold hover:opacity-90 active:opacity-80 transition-opacity ${className ?? ""}`} {...buttonProps}>
       {children}
     </button>
-  )
+  );
 }
